18cbb2b5a6856f8765608d5be8ce3448b292e6bc
Jeffrey-ch-Ng <ng.jeffrey@rogers.com>
2019-01-26 16:28:23 -0500

    Database update

--- records/models.py
+++ records/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+test_text = "Hello there guys"
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.CharField(max_length=200)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
--- records/views.py
+++ records/views.py
@@ -4,9 +4,14 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .models import Question
+from .models import Choice
 
 # Create your views here.
 def index(request):
+    all_questions = Question.objects.all()
+    context = {
+        'all_questions': all_questions,
+    }
     template = loader.get_template('records/index.html')
-    return HttpResponse(template.render())
+    return HttpResponse(template.render(context, request))
 
