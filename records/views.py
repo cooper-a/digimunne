221b456f026e6dea9704c59d431dd82aee07ea97
Jeffrey-ch-Ng <ng.jeffrey@rogers.com>
2019-01-26 15:20:43 -0500

    Enable templates

--- records/views.py
+++ records/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the records index.")
+    template = loader.get_template('records/index.html')
+    return HttpResponse(template.render())
+
